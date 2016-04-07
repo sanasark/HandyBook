401008e451a49f4b2b4090be471f54cba11068ef
Sanasar <sanasar@inbox.ru>
2016-04-07 22:36:22 +0400

    Initial Commit

---
+++ HandyBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandyBook
+//
+//  Created by User on 4/7/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HandyBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandyBook
+//
+//  Created by User on 4/7/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
