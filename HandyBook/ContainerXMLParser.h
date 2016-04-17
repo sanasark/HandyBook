a25d8f8ee3092ece2321c193b715144c183a65ad
Lilit Khachatryan <khachatryan.lilith@gmail.com>
2016-04-18 00:08:01 +0400

    Changed epub xml reading classes

---
+++ HandyBook/ContainerXMLParser.h
@@ -0,0 +1,13 @@
+//
+//  ContainerXMLParser.h
+//  HandyBook
+//
+//  Created by User on 4/17/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import "EpubXMLFilesParser.h"
+
+@interface ContainerXMLParser : EpubXMLFilesParser
+
+@end
---
+++ HandyBook/ContainerXMLParser.m
@@ -0,0 +1,13 @@
+//
+//  ContainerXMLParser.m
+//  HandyBook
+//
+//  Created by User on 4/17/16.
+//  Copyright © 2016 ACA. All rights reserved.
+//
+
+#import "ContainerXMLParser.h"
+
+@implementation ContainerXMLParser
+
+@end
